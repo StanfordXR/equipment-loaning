006d3d1a3fb776092d711a7a38ca91180d7d538a
stong88 <70980554+stong88@users.noreply.github.com>
2025-11-02 15:44:02 -0800

    chore: fix inventory page space indent to tabs

--- app/admin/inventory/page.tsx
+++ app/admin/inventory/page.tsx
@@ -5,19 +5,19 @@ import Container from '@/components/primitives/container';
 import Title from '@/components/primitives/text/title';
 
 export default async function AdminInventoryPage() {
-  const client = generateSSRClient();
+	const client = generateSSRClient();
 
-  const equipmentTypes = await client.models.EquipmentType.list({
-    selectionSet: selectionSet
-  });
-  if (equipmentTypes.errors) {
-    throw new Error(JSON.stringify(equipmentTypes.errors));
-  }
+	const equipmentTypes = await client.models.EquipmentType.list({
+		selectionSet: selectionSet
+	});
+	if (equipmentTypes.errors) {
+		throw new Error(JSON.stringify(equipmentTypes.errors));
+	}
 
-  return (
-    <Container className='pt-20'>
-        <Title>Inventory</Title>
-        <EquipmentTypeTable data={equipmentTypes.data} />
-    </Container>
-  )
+	return (
+		<Container className='pt-20'>
+			<Title>Inventory</Title>
+			<EquipmentTypeTable data={equipmentTypes.data} />
+		</Container>
+	)
 }
