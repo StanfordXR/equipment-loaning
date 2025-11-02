bfa2f4f0ddf6950e2b537999d5b457cb758ffc9b
stong88 <70980554+stong88@users.noreply.github.com>
2025-11-02 15:37:42 -0800

    feat: remove close button on toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <main>
           {children}
         </main>
-        <Toaster richColors closeButton />
+        <Toaster richColors />
       </body>
     </html>
   );
