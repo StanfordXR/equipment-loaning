00d7c83d5da8329aabd4707de7fd11fa9b386b6d
stong88 <70980554+stong88@users.noreply.github.com>
2025-11-08 01:00:55 -0800

    feat: change generateAutoMatch to take availableEquipment, requests as input isntead of querying

--- app/actions/generate-auto-match.ts
+++ app/actions/generate-auto-match.ts
@@ -1,15 +1,20 @@
 'use server'
 
-import generateSSRClient from '@/app/utils/generate-ssr-client'
 import { minimumWeightBipartiteMatch } from 'min-cost-flow';
+import { Assignment } from '@/app/admin/periods/[period_id]/requests/requests';
 
 interface PeriodAutoMatchProps {
-    periodID: string;
-}
-
-interface Match {
-    requestID: string;
-    equipmentID: string;
+    availableEquipment: {
+        equipmentTypeId: string;
+        equipmentId: string;
+    }[];
+    requests: {
+        requestId: string;
+        equipmentTypeRequests: {
+            equipmentTypeId: string;
+            rank?: number;
+        }[];
+    }[];
 }
 
 interface BipartiteEdge {
@@ -17,6 +22,8 @@ interface BipartiteEdge {
     right: string;
     weight: number;
 }
+
+const DEFAULT_RANK = 999;
 
 /**
  * Return a list of matched loanable equipment to equipment requests for a given periodID.
@@ -26,79 +33,34 @@ interface BipartiteEdge {
  * - The matching algorithm will only take into consideration Equipment objects that are
  *      referenced in at least one Request
  */
-export default async function generateAutoMatch({ periodID }: PeriodAutoMatchProps) {
-    // export default async function generateAutoMatch({ periodID }: PeriodAutoMatchProps): Promise<Match[]> {
-    const client = generateSSRClient();
+export default async function generateAutoMatch({ availableEquipment, requests }: PeriodAutoMatchProps): Promise<Assignment[]> {
+    let edges: BipartiteEdge[] = [];
 
-    const { data, errors } = await client.models.Period.get({ id: periodID }, {
-        selectionSet: [
-            'loanableEquipment.equipment.id',
-            'loanableEquipment.equipment.equipmentTypeId',
-            'loanableEquipment.equipment.assignment.id',
+    // For each request...
+    requests.map(({ requestId, equipmentTypeRequests }) => {
+        // For each equipment type request...
+        equipmentTypeRequests.map(({ equipmentTypeId, rank }) => {
+            // For each available equipment per equipment type...
 
-            'requests.equipmentTypeRequests.equipmentTypeId',
-            'requests.equipmentTypeRequests.requestId',
-            'requests.equipmentTypeRequests.rank',
-        ]
-    });
+            if (rank && rank < 1) {
+                throw `Expected rank >= 1 for request id ${requestId}, equipment type id ${equipmentTypeId}, but got ${rank}`;
+            }
+            const equipments = availableEquipment.filter(e => e.equipmentTypeId == equipmentTypeId);
 
-    if (errors) {
-        throw new Error(JSON.stringify(errors));
-    }
-
-    if (!data) {
-        throw 'Expected nonnull data, but got null';
-    }
-
-    // Get loanable (non-assigned) equipment for this period, grouped by type
-    let equipments = data.loanableEquipment.map(
-        (loanableEquipment) => loanableEquipment.equipment
-    );
-    if (equipments.length == 0) {
-        throw 'Expected at least one Equipment, but got 0';
-    }
-    let equipmentByType: { [equipmentTypeId: string]: string[] } = {};
-    equipments
-        .filter((e) => !e.assignment)
-        .map((e) => {
-            // We assume that there cannot be multiple Equipments with the same id
-            // since Amplify data dedups
-            equipmentByType[e.equipmentTypeId].push(e.id)
+            equipments.map(({ equipmentId }) => {
+                edges.push({
+                    left: requestId,
+                    right: equipmentId,
+                    weight: rank ?? DEFAULT_RANK
+                })
+            });
         });
-
-    // Get equipment type requests for this period
-    const equipmentTypeRequests = data.requests.map(
-        (request) => request.equipmentTypeRequests
-    ).flat();
-    if (equipmentTypeRequests.length == 0) {
-        throw 'Expected at least one EquipmentTypeRequest, but got 0';
-    }
-
-    const edges: BipartiteEdge[] = equipmentTypeRequests.map((equipmentTypeRequest) => {
-        // For each EquipmentTypeRequest...
-        return equipmentByType[equipmentTypeRequest.equipmentTypeId]
-            .map((equipmentId) => {
-                if (!equipmentTypeRequest.rank || equipmentTypeRequest.rank < 0) {
-                    throw `EquipmentTypeRequest with request_id ${equipmentTypeRequest.requestId}, equipment_type_id
-                    ${equipmentTypeRequest.equipmentTypeId} has invalid rank: ${equipmentTypeRequest.rank}`;
-                }
-                // Add a bipartite edge for every piece of equipment in the requested equipment type
-                return {
-                    left: equipmentTypeRequest.requestId,  // request
-                    right: equipmentId,  // equipment
-                    weight: equipmentTypeRequest.rank  // ranking
-                }
-            })
-    }).flat();
-
-    console.log(edges);
-
-    // Run bipartite matching and return
+    });
     const matching = minimumWeightBipartiteMatch(edges);
     return matching.map(edge => {
         return {
-            requestID: edge.left,
-            equipmentID: edge.right
+            requestId: edge.left,
+            equipmentId: edge.right
         };
     });
 }
