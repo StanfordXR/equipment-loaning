db78dc1fda286086ff0b5aa077fe632ecf0210b0
stong88 <70980554+stong88@users.noreply.github.com>
2025-11-08 23:55:36 -0800

    feat: add optional description to InputLabel and callsite TextInputWithLabel

--- components/primitives/interactions/input-label.tsx
+++ components/primitives/interactions/input-label.tsx
@@ -3,17 +3,26 @@ import { ReactNode } from 'react';
 
 interface LabelProps {
     label: string;
+    description?: string;
     inputID?: string;
     isRequired?: boolean;
     children: ReactNode;
 }
 
-export default function InputLabel({ label, inputID, isRequired = true, children }: LabelProps) {
+export default function InputLabel({ label, inputID, isRequired = true, children, description }: LabelProps) {
     return (
         <div className='grid w-full gap-2'>
-            <Label htmlFor={inputID} className='gap-1'>
-                {label}
-                {isRequired && <span className='text-red-500'>*</span>}
+            <Label htmlFor={inputID}>
+                <div>
+                    <div className={description ? 'mb-1' : ''}>
+                        {label} {isRequired && <span className='text-red-500'>*</span>}
+                    </div>
+                    {description &&
+                        <p className='font-normal text-muted-foreground leading-tight'>
+                            {description}
+                        </p>
+                    }
+                </div>
             </Label>
             {children}
         </div>
