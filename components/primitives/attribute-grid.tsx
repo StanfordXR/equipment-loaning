6e743bcae985c54b51f012579c8c6cfd4a88be1b
stong88 <70980554+stong88@users.noreply.github.com>
2025-11-10 02:47:16 -0800

    feat: add maxCols to AttributeGrid

--- components/primitives/attribute-grid.tsx
+++ components/primitives/attribute-grid.tsx
@@ -1,9 +1,17 @@
 import { cn } from '@/lib/utils';
 import { ChildrenAndClassName } from '../types';
 
-export default function AttributeGrid({ children, className }: ChildrenAndClassName) {
+type AttributeGridProps = ChildrenAndClassName & {
+    maxCols?: 2 | 3;
+}
+
+export default function AttributeGrid({ children, className, maxCols = 3 }: AttributeGridProps) {
     return (
-        <div className={cn('grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-4 md:gap-y-6', className)}>
+        <div className={cn(
+            'grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-4 md:gap-y-6',
+            (maxCols == 3 && 'lg:grid-cols-3'),
+            className
+        )}>
             {children}
         </div>
     );
