8ec24178deb2e18b021ddd4d1c48b7128bfe021f
stong88 <70980554+stong88@users.noreply.github.com>
2025-11-10 23:39:47 -0800

    feat: modify user current requests page to account for request status changes

--- app/requests/components/current-requests.tsx
+++ app/requests/components/current-requests.tsx
@@ -10,6 +10,7 @@ import dayjs from 'dayjs';
 import { Badge } from '@/components/ui/badge';
 import Link from 'next/link';
 import Subtext from '@/components/primitives/text/subtext';
+import { RequestStatus } from '@/amplify/data/constants';
 
 export default async function CurrentRequests() {
     const client = generateSSRClient();
@@ -56,26 +57,40 @@ export default async function CurrentRequests() {
                 <Subtext>Select a request to view details.</Subtext>
             </div>
             <div className='grid grid-cols-2 gap-4'>
-                {requests.map(request => (
-                    <Item variant='outline' key={request.id} asChild className='items-start'>
-                        <Link href={`/requests/${request.id}`}>
-                            <ItemContent>
-                                <div className='flex flex-col gap-0.5'>
-                                    <ItemTitle>Request for {request.period.name}</ItemTitle>
-                                    <ItemDescription>Created {dayjs(request.createdAt).format('M/D/YY')}</ItemDescription>
-                                </div>
-                                <div>
-                                    <Badge variant={(request.assignment && !request.status) ? 'default' : 'secondary'}>
-                                        {request.assignment ?
-                                            (request.status ?? 'Request Approved!') :
-                                            'Request pending'
-                                        }
-                                    </Badge>
-                                </div>
-                            </ItemContent>
-                        </Link>
-                    </Item>
-                ))}
+                {requests.map(request => {
+                    let badgeLabel: string;
+                    let isBadgeVariantDefault = false;
+                    if (request.pastAssignment) {
+                        badgeLabel = 'Returned';
+                    } else if (request.assignment) {
+                        if (request.status == RequestStatus.CHECKED_OUT) {
+                            badgeLabel = 'Checked out';
+                        } else if (request.status == RequestStatus.DENIED) {
+                            badgeLabel = 'Denied';
+                        } else {
+                            badgeLabel = 'Request Approved!';
+                            isBadgeVariantDefault = true;
+                        }
+                    } else {
+                        badgeLabel = 'Request Pending'
+                    }
+
+                    return (
+                        <Item variant='outline' key={request.id} asChild className='items-start'>
+                            <Link href={`/requests/${request.id}`}>
+                                <ItemContent>
+                                    <div className='flex flex-col gap-0.5'>
+                                        <ItemTitle>Request for {request.period.name}</ItemTitle>
+                                        <ItemDescription>Created {dayjs(request.createdAt).format('M/D/YY')}</ItemDescription>
+                                    </div>
+                                    <div>
+                                        <Badge variant={isBadgeVariantDefault ? 'default' : 'secondary'}>{badgeLabel}</Badge>
+                                    </div>
+                                </ItemContent>
+                            </Link>
+                        </Item>
+                    );
+                })}
             </div>
         </div >
     )
